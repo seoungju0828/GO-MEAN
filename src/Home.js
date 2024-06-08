e24f5a4a69b19dcf3a14bf954da71d7009e31fb6
seoungju0828 <ministar2006@gmail.com>
2024-06-09 01:58:53 +0900

    index.js 외의 파일 삭제

--- src/Home.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import './CSS/Home.css';
-const root = ReactDOM.createRoot(document.getElementById("root"))
-
-function Main() {
-    return <div>My First Component!</div>
-}
-
-root.render(<Main />)
