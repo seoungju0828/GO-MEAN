67d598134a230a8c4a86329889f58c27ffb0e149
seoungju0828 <ministar2006@gmail.com>
2024-06-09 02:00:00 +0900

    렌더링 방식 변경

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import "./Home.js"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// 홈화면 렌더링
+import Home from './Pages/Home.js';
+
+// 고민작성 화면 렌더링
+// import Write from './Pages/Write.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<Home />
+    	{/* <Write /> */}
+  	</React.StrictMode>
+);
